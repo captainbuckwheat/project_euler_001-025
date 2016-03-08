94e2a167a1942e8f4f70cff2bc7a684a7770a2de
AK <captainbuckwheat@gmail.com>
2016-03-07 21:18:04 -0800

    Update 022_names_scores.js

--- 022_names_scores.js
+++ 022_names_scores.js
@@ -5,3 +5,39 @@ For example, when the list is sorted into alphabetical order, COLIN, which is wo
 
 What is the total of all the name scores in the file?
 */
+<script src="names.js"></script>
+<script>
+var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var letter_number = {}; 
+for (i = 0; i<alphabet.length; i++) {
+	letter_number[alphabet[i]] = i+1; 
+}
+names.sort();
+var name_sum = function(x){
+	var sum, i; 
+	sum = 0; 
+	for (i = 0; i<x.length; i++) {
+		sum += letter_number[x[i]]; 
+	}
+	return sum; 
+}
+var sum_name_scores = function(z){
+	var i, sum; 
+	sum = 0; 
+	for (i = 0; i < z.length; i++) {
+		sum += name_sum(z[i])*(i+1);
+	}
+	return sum;
+}
+var test = function(){
+	var our_names = ["AKSANA", "DUSTY", "JOSH", "NACHO"];
+	if (letter_number["Z"] !== 26){
+		console.log('test #1 has failed');
+	} else if (sum_name_scores(our_names) !== 545){
+		console.log('test #2 has failed');
+	} else {
+		console.log('it works like a fkn clock!');
+	}
+}
+console.log(sum_name_scores(names)); 
+</script>
