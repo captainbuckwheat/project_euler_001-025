768ebe03c9b69ac534804867353d046123b5f2d1
AK <captainbuckwheat@gmail.com>
2016-05-12 19:26:47 -0700

    Update 024_lexicographic_permutations.js

--- 024_lexicographic_permutations.js
+++ 024_lexicographic_permutations.js
@@ -5,33 +5,32 @@ A permutation is an ordered arrangement of objects. For example, 3124 is one pos
 
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 */
-
+var factorial = function(x) { // for positive numbers only. 
+	var i, fact; 
+	fact = 1;
+	for (i = 2; i <= x; i++) fact*=i; 
+	return fact; 
+}
 var perm = [];
-var lexicographic_permutation = function(z) {
-	var x, y, j, a;
-	x = 3628800;
+var lexicographic_permutation = function(start, finish, index) { //consecutive numbers  
+	var x, y, j, a, len, prev_factorial;
+	len = finish - start + 1; 
+	x = index + 1;
+	a = [];
 	for (i = 0; i < x; i++) perm[i]=[];
 	for (j = 0; j < x; j++) {
-		a = [0,1,2,3,4,5,6,7,8,9];
-		perm[j][0] = a.splice(Math.floor(j/362880), 1)[0];
-		y = j%362880;
-		perm[j][1] = a.splice(Math.floor(y/40320), 1)[0];
-		y = j%40320;
-		perm[j][2] = a.splice(Math.floor(y/5040), 1)[0];
-		y = j%5040; 
-		perm[j][3] = a.splice(Math.floor(y/720), 1)[0];
-		y = j%720;
-		perm[j][4] = a.splice(y/120, 1)[0];
-		y = j%120; 
-		perm[j][5] = a.splice(y/24, 1)[0];
-		y = j%24;
-		perm[j][6] = a.splice(Math.floor(y/6), 1)[0];
-		y = j%6; 
-		perm[j][7] = a.splice(Math.floor(y/2), 1)[0];
-		y = j%2;
-		perm[j][8] = a.splice(y, 1)[0];
-		y = j; 
-		perm[j][9] = a.splice(0, 1)[0];
+		prev_factorial = 1; 
+		for (k = start; k <= finish; k++) a[k] = k; 
+		perm[j][start] = a.splice(Math.floor(j/factorial(len-prev_factorial)), 1)[0];
+		y = j%factorial(len-prev_factorial);
+		prev_factorial++;
+		for (k = start+1; k <= finish; k++){
+			perm[j][k] = a.splice(Math.floor(y/factorial(len-prev_factorial)), 1)[0];
+			y = j%factorial(len-prev_factorial);
+			prev_factorial++;
+		}
 	}
-	return perm[z];
+	return perm[index-1];
 };
+
+
